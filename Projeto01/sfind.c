8c7247faf303b87b268accad5d364a8736699e35
anaezes <anaezes@gmail.com>
2017-04-11 00:39:24 +0100

    Project: added -type option

--- Projeto01/sfind.c
+++ Projeto01/sfind.c
@@ -7,33 +7,77 @@
 #include <sys/stat.h> 
 #include <sys/types.h>
 
+#define TYPE_FILE 0
+#define TYPE_DIR 1
+
 void sigHandler(int signo)
 {
+	switch(signo)
+	{
+		case SIGINT:
+		{
+			char terminate[3];
 
-  switch(signo)
-  {
-    case SIGINT:
-    {
-          char terminate[3];
-
-          while (1) {
-            write(STDOUT_FILENO, "Are you sure you want to terminate (Y/N)?", 42);
-            read(STDIN_FILENO, &terminate,3);
+			while (1) {
+				write(STDOUT_FILENO, "Are you sure you want to terminate (Y/N)?", 42);
+				read(STDIN_FILENO, &terminate,3);
 
             // If answer Y -> finish. Otherwise, re-ask, unless answer is N.
-            if( terminate[1] == '\n' ) {
-                if( terminate[0]=='Y' || terminate[0]=='y' )
+				if( terminate[1] == '\n' ) {
+					if( terminate[0]=='Y' || terminate[0]=='y' )
                     exit(EXIT_FAILURE); //alterei porque estava a originar imensos zoombies
                 else if (terminate[0]=='N' || terminate[0]=='n')
-                    return;
+                	return;
             }
             else
-                write(STDOUT_FILENO, "Answer not accepted\n", 21);
-          }
-          break;
-    }
-  }
+            	write(STDOUT_FILENO, "Answer not accepted\n", 21);
+        }
+        break;
+		}
+	}
+}
 
+
+/**
+* Verify type of files required by command 
+* @return 0 if file, 1 if directory or -1 if not 
+* specified or invalid option
+**/
+int parse_type(int argc, char ** argv) {
+	if(argc < 4)
+		return -1;
+
+	int found = 0;
+	int return_value = -1;
+	int i = 0;
+	while(!found && i < argc) {
+		if((strcmp(argv[i], "-type") == 0) && (i+1 < argc)) {
+			found = 1;
+			if(strcmp(argv[i+1], "f") == 0)
+				return_value = TYPE_FILE;
+			else if(strcmp(argv[i+1], "d") == 0)
+				return_value = TYPE_DIR;
+		}
+		i++;
+	}
+
+	return return_value;
+}
+
+/**
+* Build new command with new directory
+* @return new command
+**/
+char* get_new_args(char* dirName, int argc, char ** argv) {
+	char *str = malloc(512);
+	strcpy(str, dirName);
+
+	for(int i = 2; i < argc; i++){
+		strcat(str, " ");
+		strcat(str, argv[i]);
+	}
+
+	return str;
 }
 
 
@@ -50,46 +94,37 @@ int main(int argc, char ** argv)
 		exit(1);
 	}
 
-	if (argc < 2)
+	if (argc < 2) {
 		printf("This program requires at least 1 argument (directory), example ./sfind / \n"); 
+		exit(1);
+	}
 
-
-  	//try find
+	int type_option = parse_type(argc, argv);
 
 	DIR *directory; 
 	directory = opendir(argv[1]); 
 	struct dirent *curr_node;
-	struct stat status;
-
-
-	while((curr_node = readdir(directory)) != NULL)
+	
+	while((curr_node = readdir(directory)) != NULL )
 	{
-		printf("%s\n",(curr_node)->d_name); 
-		stat((curr_node)->d_name,&status); 
-		char *dirName;
-		switch(status.st_mode & S_IFMT) { 
+		char *dirName = (curr_node)->d_name;
 
-			case(S_IFREG): 
-				//printf("FILE;\n");
-				// do something to regular files ???
-			break; 
-
-			case (S_IFDIR): 
-				//printf("DIR;\n");
-				dirName = (curr_node->d_name);
-
-			if (dirName[0] == '.' && (dirName[1] == '\0' || dirName[1] == '.'))
-				break; 
-			pid_t ppid = getpid();
-
-			if (fork() == 0){ 
-				//printf("\nOpening %s\n ", dirName); 
-				execlp("./sfind","./sfind", dirName, NULL); 
-				//printf("EXEC FAILED! ABORT!\n");
-			} 
-			break; 
-		} 
-		usleep(1000);
+		//print type file or both
+		if(curr_node->d_type == DT_REG && (type_option == TYPE_FILE || type_option == -1))
+			printf("F: %s\n", dirName);
+		else if(curr_node->d_type == DT_DIR) {
+			//not print this
+			if(strcmp(dirName, ".") == 0 || strcmp(dirName, "..") == 0)
+				continue;
+			//print type dir or both
+			if(type_option == TYPE_DIR || type_option == -1) 
+				printf("D: %s\n", dirName);
+			//sfind in DIR's
+			if (fork() == 0){
+				execlp("./sfind","./sfind", get_new_args(dirName, argc, argv), NULL); 
+				printf("EXEC FAILED! ABORT!\n");
+			}
+		}
 	} 
 
 	return 0;
