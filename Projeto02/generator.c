4632164c5c13d2426616dcd213057521b1d04579
Luís Martins <luis.n.martins97@gmail.com>
2017-05-01 17:11:36 +0100

    comments added

--- Projeto02/generator.c
+++ Projeto02/generator.c
@@ -4,24 +4,27 @@
 #include <pthread.h>
 
 typedef struct arg_struct {
-    int arg1;
-    int arg2;
+    int nmbOfOrders;
+    int maximumTime;
 }args;
 
 
 void *generator(void * arguments){
 	
+	//ge
 	args* new_args = (args*) arguments;
 	char genders[] = {'M', 'W'};
+	
 	//rand
 	time_t t;
 	//install rand seed
+	
 	srand((unsigned) time(&t));
 
-	for(int i=0; i<new_args->arg1; i++){
+	for(int i=0; i<new_args->nmbOfOrders; i++){
 
 
-		printf("It: %d  , time: %d   ,   gender: %c\n", i, rand()%new_args->arg2, genders[rand()%2]);
+		printf("It: %d  , time: %d   ,   gender: %c\n", i, rand()%new_args->maximumTime, genders[rand()%2]);
 
 	}
 
@@ -33,12 +36,15 @@ void *generator(void * arguments){
 
 int main(int argc, char** argv){
 	
-
 	pthread_t generatorTID;
 	int pthread_res;
-	args* generator_args = (args*) malloc(sizeof(args)); 
-	generator_args->arg1 = atoi(argv[1]);
-	generator_args->arg2 = atoi(argv[2]);
+
+	//create an agrs struct to save values to be used in thread creation
+	args* generator_args = (args*) malloc(sizeof(args));
+	generator_args->nmbOfOrders = atoi(argv[1]);
+	generator_args->maximumTime = atoi(argv[2]);
+
+	//create thread
 	if((pthread_res = pthread_create(&generatorTID, NULL, &generator, (void *)generator_args)) != 0){
 		printf("Error creating generator's thread: %s", strerror(pthread_res));
 
