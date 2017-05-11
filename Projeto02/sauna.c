8628ac42c363481fd1ff0642f132d1807b3f76f9
anaezes <anaezes@gmail.com>
2017-05-11 12:03:03 +0100

    sauna: fixed bugs with semaphore

--- Projeto02/sauna.c
+++ Projeto02/sauna.c
@@ -9,10 +9,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "request.h"
-
 #include <semaphore.h>
+
 int freeSeats, saunaSpaces;
 sem_t* sem_sauna;
+
+typedef struct thread_struct{
+	int nRequests;		
+	pthread_t* threads;	
+} request_threads;
+
 
 /**
  * Function used to create and set the FIFO's, by directing them accordingly.
@@ -28,8 +34,8 @@ int confFifos (int* fd) {
 	const char* entryFifo = FIFO_ENTRADA;
 
 	//Creating both FIFO's
-  createFifo(entryFifo);
-  createFifo(exitFifo);
+	createFifo(entryFifo);
+	createFifo(exitFifo);
 
 	//Setting the Fifo's 'Flow'
 	printf("Waiting for generator.c to begin.\n");
@@ -49,7 +55,7 @@ int confFifos (int* fd) {
 
 float timedifference_msec(struct timeval t0, struct timeval t1)
 {
-    return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
+	return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 }
 
 //Gerador de multi threads, cada um para cada novo pedido que conté a struct x.
@@ -61,30 +67,29 @@ void* saunaHandler(void* args){
 	sem_getvalue(sem_sauna, &tmp);
 	printf("LUGARES VAZIOS: %d\n", tmp);
 
-
 	freeSeats--;
 	request* new_request = (request*) args;
 	printf("Request ID: %d ESTA na sauna\n", new_request->rid);
 
-  	struct timeval start_time, curr_time;
-  	int elapsed;
+	struct timeval start_time, curr_time;
+	int elapsed;
 
-   	gettimeofday(&start_time, 0);
-   	usleep((new_request->time)*1000);
-   	gettimeofday(&curr_time, 0);
+	gettimeofday(&start_time, 0);
+	usleep((new_request->time)*1000);
+	gettimeofday(&curr_time, 0);
 
-   	elapsed = timedifference_msec(start_time, curr_time);
+	elapsed = timedifference_msec(start_time, curr_time);
 
-   	printf("Request ID: %d SAIU da sauna, em %d milliseconds.\n", new_request->rid, elapsed);
+	printf("Request ID: %d SAIU da sauna, em %d milliseconds.\n", new_request->rid, elapsed);
 
-    free(args);
+	free(args);
 
 	pthread_exit(NULL);
 
 	freeSeats++;
 	sem_post(sem_sauna);
 
-    return NULL;
+	return NULL;
 }
 
 
@@ -101,11 +106,12 @@ void send_confirmation(int* fd) {
 }
 
 //função de processo de decisão
-int requestDecision(request* curr_request,char* gender, int* fd, struct timeval start_time){
+int requestDecision(request* curr_request, char* gender, int* fd, struct timeval start_time, request_threads* threadsInfo){
 	
 	struct timeval curr_time;
 	gettimeofday(&curr_time, 0);
 	char tip[9];
+
 	//write activity
 	strcpy(tip, "RECEBIDO");
 	writeActivity(fd, timedifference_msec(start_time, curr_time), curr_request, getpid(), getpid(), tip, 'S');
@@ -113,29 +119,35 @@ int requestDecision(request* curr_request,char* gender, int* fd, struct timeval 
 
 	if(*gender == NO_GENDER || curr_request->gender == *gender){
 		printf("\n\nRequest ID: %d is on, with time: %d\n", curr_request->rid, curr_request->time);
+
 		//create detached thread
 		pthread_t new_user_tid;
 		int pthread_res;
 		pthread_attr_t new_user_attr;
 		pthread_attr_init(&new_user_attr); // get default pthread definitions
-		pthread_attr_setdetachstate(&new_user_attr, PTHREAD_CREATE_DETACHED); // set thread as detached
+		pthread_attr_setdetachstate(&new_user_attr, PTHREAD_CREATE_JOINABLE);
+		//pthread_attr_setdetachstate(&new_user_attr, PTHREAD_CREATE_DETACHED); // set thread as detached
 
 		if((pthread_res = pthread_create(&new_user_tid, &new_user_attr, &saunaHandler, (void *)curr_request)) != TRUE){
 			printf("Error creating generator's thread: %s", strerror(pthread_res));
 			return FALSE;
 		}
+
+		//save thread
+		threadsInfo->threads[threadsInfo->nRequests] = new_user_tid;
+		threadsInfo->nRequests++;
 		
 		//write activity
 		strcpy(tip, "SERVIDO");
-	  	gettimeofday(&curr_time, 0);
-	  	writeActivity(fd, timedifference_msec(start_time, curr_time), curr_request, getpid(), pthread_res, tip, 'S');
+		gettimeofday(&curr_time, 0);
+		writeActivity(fd, timedifference_msec(start_time, curr_time), curr_request, getpid(), pthread_res, tip, 'S');
 		
 		//update sauna gender
 		if(freeSeats == saunaSpaces)
 			*gender = NO_GENDER;
 		else
 			*gender = curr_request->gender;
- 		
+
 		return TRUE;
 	}
 	else
@@ -143,13 +155,13 @@ int requestDecision(request* curr_request,char* gender, int* fd, struct timeval 
 		//write activity
 		strcpy(tip, "REJEITADO");
 		gettimeofday(&curr_time, 0);
-	  	writeActivity(fd, timedifference_msec(start_time, curr_time), curr_request, getpid(), getpid(), tip, 'S');
+		writeActivity(fd, timedifference_msec(start_time, curr_time), curr_request, getpid(), getpid(), tip, 'S');
 
 		printf("\n\nRequest ID DENIED: %d\n", curr_request->rid);
 		writeRequest(curr_request, fd);
 		return FALSE;
 	}
-		
+
 	
 }
 
@@ -185,18 +197,17 @@ int main (int argc, char** argv) {
 		printf("Error on function confFifos().\n");
 		exit(2);
 	} else
-		printf("Successfuly established connection to generator.c.\n\n");
+	printf("Successfuly established connection to generator.c.\n\n");
 
 
 	//open activity file
-
 	int activity_fd;
 	
 	if((activity_fd = openActivityFile('S')) == FALSE){
 		printf("Error opening sauna's activity file\n");
 		exit(2);
 	}else
-		printf("Sauna's activity file was successfuly opened.\n\n");	
+	printf("Sauna's activity file was successfuly opened.\n\n");	
 
 	//Installing atexitHandler
 	atexit(destroyFifos);
@@ -207,11 +218,14 @@ int main (int argc, char** argv) {
 	//Ensure that the semaph does not exist
 	sem_unlink("/sem_sauna");
 	sem_sauna = sem_open("/sem_sauna", O_CREAT, SEMAPHORE_MODE, saunaSpaces);
-     if(sem_sauna == SEM_FAILED) {
-       perror("Error to create semaphore for sauna.");
-       exit(3);
+	if(sem_sauna == SEM_FAILED) {
+		perror("Error to create semaphore for sauna.");
+		exit(3);
 	}
 
+	request_threads threadsInfo;
+	threadsInfo.nRequests = 0; 
+	threadsInfo.threads = malloc(1024*sizeof(pthread_t));
 
 	request* rReq;
 	while (1) {
@@ -228,12 +242,17 @@ int main (int argc, char** argv) {
 		if ((rReq->rid % 4) == 0)
 			writeRequest(rReq, fd);
 		else{
-			requestDecision(rReq, &gender, &activity_fd, start_time);
+			requestDecision(rReq, &gender, &activity_fd, start_time, &threadsInfo);
 			send_confirmation(fd);
 		}
 	}
 
-	//atexit handller que chama a destroyFifos?? Parece-me bem e lógico, perguntar ao prof na sexta tb
+	int i = 0; 
+	while(i < threadsInfo.nRequests) {
+		pthread_join(threadsInfo.threads[i], NULL);
+		printf("i: %d \n", i);
+		i++;
+	}
 
 	exit(0);
 }
